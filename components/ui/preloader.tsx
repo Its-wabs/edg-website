4e2bace7fca452498258cfbb459afaa10af6cd1b
thorfinn <cherfi.m.abdelwahab@gmail.com>
2026-04-01 18:32:39 +0100

    style: centering the the items on mobile

--- components/ui/preloader.tsx
+++ components/ui/preloader.tsx
@@ -46,7 +46,7 @@ const PreLoad = ({ onComplete }: PreloaderProps) => {
 
           gsap.set(barTrackRef.current, {
             rotation: 90,
-            x: isDesktop ? 55 : 40,
+            x: isDesktop ? 55 : 45,
           })
           gsap.set(textRef.current, { x: -10 })
 
