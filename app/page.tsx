fa5a2c10ebcb0ac2a7873c536fe87e18bf5b841c
thorfinn <cherfi.m.abdelwahab@gmail.com>
2026-04-07 14:16:40 +0100

    style: scaled down the process/philosophy items

--- app/page.tsx
+++ app/page.tsx
@@ -391,7 +391,7 @@ export default function Home() {
               .from(
                 process.querySelectorAll(steps),
                 {
-                  scale: isDesktop ? 0.8 : 0.95,
+                  scale: isDesktop ? 0.8 : 0.6,
                   y: isDesktop ? 0 : 20,
                   opacity: 0,
                   stagger: 0.2,
